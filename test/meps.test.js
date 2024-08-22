3db507ee58c3b61eb0bfe2379b07b9794ea76661
Filipe Catraia <filipe@flowchase.app>
2024-08-22 13:02:21 +0200

    comments on tests

--- test/meps.test.js
+++ test/meps.test.js
@@ -5,14 +5,16 @@ test('legislators', async () => {
 
   // This number can fluctuate, feel free to update the test if it's the case.
   expect(Array.isArray(legislators)).toBeTruthy()
-  expect(legislators.length).toBe(719)
+  expect(legislators.length).toBe(719) // @todo length has changed!
 
-  const first = legislators[2]
+  // @todo first legislator is no longer ‘pro-Chat Control’ Magdalena
+  const first = legislators[2]g
   expect(first.name).toBe('Magdalena ADAMOWICZ')
   expect(first.lastName).toBe('ADAMOWICZ')
   expect(first.partyGroup).toBe('Group of the European People\'s Party (Christian Democrats)')
   expect(first.country).toBe('Poland')
-  // expect(first.url).toBe('https://www.europarl.europa.eu/meps/en/197490/MAGDALENA_ADAMOWICZ/home') @todo
+  // @todo URL format has changed?
+  // expect(first.url).toBe('https://www.europarl.europa.eu/meps/en/197490/MAGDALENA_ADAMOWICZ/home')
   expect(first.url).toBe('https://www.europarl.europa.eu/meps/en/197490')
   expect(first.image).toBe('https://www.europarl.europa.eu/mepphoto/197490.jpg')
 })
